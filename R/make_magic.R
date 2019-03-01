618bc2a8b8ae997e193c9f3b996345a66c5d3225
Sarah Odell <sgo7@cornell.edu>
2019-02-28 16:03:04 -0800

    making g_map argument for functions

--- R/make_magic.R
+++ R/make_magic.R
@@ -3,20 +3,21 @@
 #' @param start_pop A list of chromosome objects from the starting lines of the MAGIC, should be an even number of lines
 #' and listed in the order that crossing should occur
 #' @param c Chromosome number (default is 10)
-#' @param map The genetic map
+#' @param g_map The genetic map
+#'
 #' @return A list of chromosome objects that are a result of funnel crossing the lines in start_pop
 #' @export
 
 
-make_magic<-function(start_pop,c=10,map){
+make_magic<-function(start_pop,c=10,g_map){
   if(length(start_pop)==2){
-    return(offspring(start_pop[[1]],start_pop[[2]],c,map))
+    return(offspring(start_pop[[1]],start_pop[[2]],c,g_map))
   }
   magic=list()
   count=1
   for(i in seq(1,length(start_pop),2)){
-    magic[[count]]=offspring(start_pop[[i]],start_pop[[i+1]],c,map)
+    magic[[count]]=offspring(start_pop[[i]],start_pop[[i+1]],c,g_map)
     count=count+1
   }
-  return(make_magic(magic,c,map))
+  return(make_magic(magic,c,g_map))
 }
--- R/outcross.R
+++ R/outcross.R
@@ -3,11 +3,13 @@
 #' @param pop Starting population, pop[[i]] is the output of function offspring (list of lists)
 #' @param ngen The number of generations of outcrossing
 #' @param c The chromosome being simulated
+#' @param g_map The genetic map
+#'
 #' @return A synthetic population of the same size as the initial population, pop that
 #' has been randomly outcrossed for ngen generations (list of lists)
 #' @export
 
-outcross<-function(pop,ngen,c){
+outcross<-function(pop,ngen,c,g_map){
   size=length(pop)
   new_pop=list()
   if(ngen==1){
@@ -16,7 +18,7 @@ outcross<-function(pop,ngen,c){
     for(p in seq(1,size*2,2)){
       draw1=draw[p]
       draw2=draw[p+1]
-      new_pop[[count]]=offspring(pop[[draw1]],pop[[draw2]],c)
+      new_pop[[count]]=offspring(pop[[draw1]],pop[[draw2]],c,g_map)
       count=count+1
     }
     return(new_pop)
@@ -26,7 +28,7 @@ outcross<-function(pop,ngen,c){
   for(p in seq(1,size*2,2)){
     draw1=draw[p]
     draw2=draw[p+1]
-    new_pop[[count]]=offspring(pop[[draw1]],pop[[draw2]],c)
+    new_pop[[count]]=offspring(pop[[draw1]],pop[[draw2]],c,g_map)
     count=count+1
   }
   return(outcross(new_pop,ngen-1,c))
