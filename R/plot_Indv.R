8762f7c76a70a5875339de3a3272237710f9d777
Sarah Odell <sgo7@cornell.edu>
2019-09-26 12:25:07 -0700

    fix Indv_plot

--- R/plot_Indv.R
+++ R/plot_Indv.R
@@ -11,7 +11,8 @@ plot_Indv <- function(breaktable,het=T){
   if(het==F){
     breaktable=breaktable[breaktable$hom=="H1",]
   }
-  breaktable$chr_f=factor(sprintf("%s_%s",breaktable$chr,breaktable$hom),levels=c(sprintf("%s_%s",breaktable$chr,breaktable$hom)))
+  chr_f=sprintf("%.0f_%s",breaktable$chr,breaktable$hom)
+  breaktable$chr_f=factor(chr_f,levels=unique(chr_f))
   p <- ggplot(breaktable,aes(x=start/1e6,y=1,color=donor)) +
     geom_segment(aes(xend=end/1e6,color=donor,yend=1),lineend="butt",size=10) +
     facet_grid(chr_f ~ .) +
