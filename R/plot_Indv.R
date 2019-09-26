2e1f3aa5fa36ee828dc3b2c294d11c0c55f9e6b1
Sarah Odell <sgo7@cornell.edu>
2019-09-26 12:18:02 -0700

    Update plot_Indv.R

--- R/plot_Indv.R
+++ R/plot_Indv.R
@@ -11,10 +11,10 @@ plot_Indv <- function(breaktable,het=T){
   if(het==F){
     breaktable=breaktable[breaktable$hom=="H1",]
   }
-  breaktable$chr=sprintf("%s_%s",breaktable$chr,breaktable$hom)
+  breaktable$chr_f=factor(sprintf("%s_%s",breaktable$chr,breaktable$hom))
   p <- ggplot(breaktable,aes(x=start/1e6,y=1,color=donor)) +
     geom_segment(aes(xend=end/1e6,color=donor,yend=1),lineend="butt",size=10) +
-    facet_grid(chr ~ .) +
+    facet_grid(chr_f ~ .) +
     ggtitle("Chromosome Breakpoints") +
     xlab("Position (Mb)")  +
     theme(axis.text.y=element_blank(),axis.title.y=element_blank(),axis.ticks.y=element_blank())
