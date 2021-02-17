7b686d142b4fd468b5450c2dbd72774d6105a0d6
Sarah Odell <sgo7@cornell.edu>
2021-02-17 12:42:26 -0800

    fix make_dh

--- R/make_dh.R
+++ R/make_dh.R
@@ -8,20 +8,20 @@
 #'
 #' @return An Indv object that is completely homozygous
 #' @export
-#' 
+#'
 
 make_dh <- function(indv,c=10,g_map){
   new_dh=Indv(nChr=c,chromlist = vector("list",length=c))
   for(i in 1:c){
     if(runif(1)>=0.5){
-      h=indv[c]@h1
+      h=indv[i]@h1
     }
     else{
-      h=indv[c]@h2
+      h=indv[i]@h2
     }
-    new_dh@chromlist[[c]]=chrom_init(i,g_map,h1_breakpoints=h@breakpoints,h1_donors = h@donors, h2_breakpoints = h@breakpoints, h2_donors = h@donors)
-    new_dh@chromlist[[c]]@h1@xo_no=length(new_dh[c]@h1@breakpoints-1)
-    new_dh@chromlist[[c]]@h2@xo_no=length(new_dh[c]@h2@breakpoints-1)
+    new_dh@chromlist[[i]]=chrom_init(i,g_map,h1_breakpoints=h@breakpoints,h1_donors = h@donors, h2_breakpoints = h@breakpoints, h2_donors = h@donors)
+    new_dh@chromlist[[i]]@h1@xo_no=length(new_dh[i]@h1@breakpoints-1)
+    new_dh@chromlist[[i]]@h2@xo_no=length(new_dh[i]@h2@breakpoints-1)
   }
 
   return(new_dh)
@@ -45,7 +45,7 @@ make_dh_pop <- function(start_pop,n=NULL,c=10,g_map){
     n=start_pop@nIndv
   }
   draw=sample(seq(1,start_pop@nIndv),n,replace=F)
-  
+
   dh_pop=Pop(nIndv=n,indvlist=vector("list",length=n))
   for(d in seq(1,n)){
     line=draw[d]
